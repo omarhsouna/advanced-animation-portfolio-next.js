b880d8569411a77f21151ea496c70b19e72b807e
omarhsouna <omar.hsouna@sesame.com.tn>
2024-09-13 11:06:57 +0100

    integrate animation projects section

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { ComponentPropsWithoutRef } from "react";
 import grainImage from "@/assets/images/grain.jpg";
 import { twMerge } from "tailwind-merge";
-interface Props {
-  className?: string;
-  children?: React.ReactNode;
-}
-const Card = ({ className, children }: Props) => {
+const Card = ({
+  className,
+  children,
+  ...rest
+}: ComponentPropsWithoutRef<"div">) => {
   return (
     <div
       className={twMerge(
         "bg-gray-800 rounded-3xl z-0 overflow-hidden relative after:content-[''] after:z-10 after:absolute after:inset-0 after:outline-2 after:outline after:outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none",
         className
       )}
+      {...rest}
     >
       <div
         className="absolute inset-0 -z-10 opacity-5"
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -57,10 +57,11 @@ export const ProjectsSection = () => {
         />
 
         <div className="flex flex-col gap-20 mt-10 md:mt-20">
-          {portfolioProjects.map((project) => (
+          {portfolioProjects.map((project, index) => (
             <Card
               key={project.title}
-              className="px-8 md:px-10 lg:pt-16 lg:px-20  pt-8 pb-0 md:pt-12"
+              className="px-8 md:px-10 lg:pt-16 lg:px-20  pt-8 pb-0 md:pt-12 sticky"
+              style={{ top: `calc(64px + ${index * 40}px)` }}
             >
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
